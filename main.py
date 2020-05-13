4c781b574c4fdfbaabdb3d68084b8b43f809e256
eirillsh <eirill.strand.hauge@gmail.com>
2020-05-13 22:24:46 +0200

    producing results

--- main.py
+++ main.py
@@ -1,38 +1,101 @@
 from hartree_fock import HartreeFock
 from moeller_plesset import MoellerPlesset as MøllerPlesset
 from hydrogen_like import HydrogenOrbital
-from harmonic_oscillator import HarmonicOscillator
 import numpy as np 
-
-print("\nHELIUM")
-Z = 2
-orbitals = HydrogenOrbital(Z)
-He = HartreeFock(orbitals, Z)
-He.solve()
-HF = He.energy()
-
-He = MøllerPlesset(orbitals, Z)
-He.solve()
-MP = He.energy()
-print(f"Experimental   energy is -2.904")
-print(f"Møller-Plesset energy is {MP}")
-print(f"Hartree-Fock   energy is {HF}")
+import matplotlib.pyplot as plt
 
 
 
-print("\n\nBERYLLIUM")
-Z = 4
-orbitals = HydrogenOrbital(Z)
-Be = HartreeFock(orbitals, Z)
-Be.solve()
-HF = Be.energy()
+def plot_MO_energies(E, atom, Z):
+	x = range(len(E))
+	y_tick = [E[i] for i in range(0, len(E), 2)]
+	plt.plot(x[:Z], E[:Z], ".", color="black", markersize=15)
+	plt.plot(x[Z:], E[Z:], ".", color="darkgrey", markersize=15)
+	plt.xticks(x, [r"$\psi_%d$" %i for i in x], fontsize=17)
+	plt.yticks(y_tick, ["%.3f" %y for y in y_tick], fontsize=15)
+	plt.grid()
+	plt.xlabel(r"$\psi_i$", fontsize=20)
+	plt.ylabel(r"MO energy [a.u]", fontsize=20)
+	plt.tight_layout()
+	plt.savefig("results/" + atom + ".pdf")
+	plt.close()
 
-Be = MøllerPlesset(orbitals, Z)
-Be.solve()
-MP = Be.energy()
-print(f"Experimental   energy is -14.67")
-print(f"Møller-Plesset energy is {MP}")
-print(f"Hartree-Fock   energy is {HF}")
+def plot_AO_energies(basis, atom):
+	x = np.linspace(0, 5, 6, dtype=int)
+	y_tick = [basis.energy(i) for i in range(0, 6, 2)]
+	x_tick  = [r"$\phi_{1s\uparrow}$", r"$\phi_{1s\downarrow}$"]
+	x_tick += [r"$\phi_{2s\uparrow}$", r"$\phi_{2s\downarrow}$"]
+	x_tick += [r"$\phi_{3s\uparrow}$", r"$\phi_{3s\downarrow}$"]
+	plt.plot(x, basis.energy(x), ".", color="black", markersize=15)
+	plt.xticks(x, x_tick, fontsize=17)
+	plt.yticks(y_tick, ["%.3f" %y for y in y_tick], fontsize=15)
+	plt.grid()
+	plt.xlabel(r"$\phi_\alpha$", fontsize=20)
+	if atom == "H":
+		plt.ylabel(r"AO energy $\frac{\mathrm{[a.u]}}{Z^2}$", fontsize=20)
+	else:
+		plt.ylabel(r"AO energy [a.u]", fontsize=20)
+	plt.tight_layout()
+	plt.savefig("results/AO_" + atom + ".pdf")
+	plt.show()
+
+
+with open("results/table.txt", "w") as outfile:
+	outfile.write("Bonding Energy:\n")
+	print("\nHELIUM")
+	Z = 2
+	orbitals = HydrogenOrbital(Z)
+	He = HartreeFock(orbitals, Z)
+	He_phi = He.energy()
+	print(f"Before HF      energy is {He_phi}")
+	He.solve()
+	He_HF = He.energy()
+	print(f"Hartree-Fock   energy is {He_HF}")
+
+	He = MøllerPlesset(orbitals, Z)
+	He.solve()
+	He_MP = He.energy()
+	print(f"Møller-Plesset energy is {He_MP}")
+	print(f"Experimental   energy is -2.904")
+	outfile.write("\t\t$\\mathrm{He}$ & %.4f & %.4f & %.4f & -2.904\\\\" % (He_phi, He_HF, He_MP))
+
+
+	print("\n\nBERYLLIUM")
+	Z = 4
+	orbitals = HydrogenOrbital(Z)
+	Be = HartreeFock(orbitals, Z)
+	Be_phi = Be.energy()
+	print(f"Before HF      energy is {Be_phi}")
+	Be.solve()
+	Be_HF = Be.energy()
+	print(f"Hartree-Fock   energy is {Be_HF}")
+
+	Be = MøllerPlesset(orbitals, Z)
+	Be.solve()
+	Be_MP = Be.energy()
+	print(f"Møller-Plesset energy is {Be_MP}")
+	print(f"Experimental   energy is -14.67")
+	outfile.write("\n\t\t$\\mathrm{Be}$ & %.3f & %.3f & %.3f & -14.67" % (Be_phi, Be_HF, Be_MP))
+
+	outfile.write("\n\nRelative errors:\n")
+	E = -2.904
+	HF = (E - He_HF)/E*100
+	MP = (E - He_MP)/E*100
+	outfile.write("\t\t$\\mathrm{He} $ & %.2f  & %.2f \\\\" % (HF, MP))
+	E = -14.67
+	HF = (E - Be_HF)/E*100
+	MP = (E - Be_MP)/E*100
+	outfile.write("\n\t\t$\\mathrm{Be} $ & %.2f & %.2f " % (HF, MP))
+
+
+
+
+plot_MO_energies(He.E, "He", 2)
+plot_MO_energies(Be.E, "Be", 4)
+
+#plot_AO_energies(HydrogenOrbital(2), "He")
+#plot_AO_energies(HydrogenOrbital(4), "Be")
+plot_AO_energies(HydrogenOrbital(1), "H")
 
 
 
